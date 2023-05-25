1fde96df5f3635172886b62b20b91f08f23d6c6f
Hoàng Trần <121411652+PainnIrene@users.noreply.github.com>
2023-05-26 02:43:31 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [message,setMessage]=useState("");
   useEffect(()=>{
-fetch("http://localhost:8000/message")
+fetch("https://testdeploy-be.onrender.com/message")
 .then((res)=>res.json())
 .then((data)=>setMessage(data.message))
   });
